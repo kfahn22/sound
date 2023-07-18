f2ec5476bddf2f1a7935e7fcb11f5bb80a42eaa4
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-07-18 07:39:54 -0400

    rename file folders and add gitignore

--- p5/dots/sketch.js
+++ p5/dots/sketch.js
@@ -9,11 +9,9 @@ const sc = 80;
 let z = -10;
 let ellipseWidth = 10;
 
-
-
 function preload() {
-  //song = loadSound("house_in_a_forest.mp3");
-  song = loadSound("bright.m4a");
+  //song = loadSound("sound_assets/house_in_a_forest.mp3");
+  song = loadSound("sound_assets/bright.m4a");
 }
 
 function setup() {
@@ -46,30 +44,30 @@ function draw() {
   rotate(ang);
 
   for (let i = 0; i < spectrum.length; i++) {
-    angle = map(i, 0, spectrum.length, 0, 360);
+    angle = map(i, 0, spectrum.length, 0, 1080);
 
     let amp = spectrum[i];
     let sc = map(amp, 0, 256, 0, 250);
     let l = map(amp, 0, 256, 20, 90);
-    let h = map(amp, 0, 256, 0, 60);
-  
+    //let h = map(amp, 0, 256, 0, 60);
+    let h = map(amp, 0, 256, 0, 360);
+
     let x = sc * cos(angle);
     let y = sc * sin(angle);
-    // strokeWeight(i/spectrum.length);
-    // stroke(h, 100, l, 10);
-    // fill(h, 100, l, 10);
-    // line(0, 0, x, y);
-    // ellipse(x, y, 6, 6);
+    strokeWeight(i / spectrum.length);
+    stroke(h, 100, l, 10);
+    fill(h, 100, l, 10);
+    line(0, 0, x, y);
+    ellipse(x, y, 6, 6);
     let ratio = x / y;
     push();
     for (let j = 0; j < 360; j += 90) {
       rotate(j);
-      //strokeWeight(i/spectrum.length);
       strokeWeight(3);
-      //stroke(h, 100, l);
+      stroke(h, 100, 1);
       fill(h, 100, l, 10);
-      ellipse(0, ratio * 8, 2 * x, y);
-      ellipse(0, ratio * 8, x, y);
+      //ellipse(0, ratio * 8, 2 * x, y);
+      //ellipse(0, ratio * 8, x, y);
     }
     pop();
   }
